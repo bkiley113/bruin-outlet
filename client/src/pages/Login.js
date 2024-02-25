f5294802cfca398ebf756499a12348358be978e4
Brandon Kiley <b.kiley.113@gmail.com>
2024-02-24 21:50:27 -0800

    Added a good amount of detail to the login and make account pages. They are linked to each other.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
@@ -7,7 +8,10 @@ const Login = () => {
             <form>
                 <input type ="text" placeholder='username'/>
                 <input type ="password" placeholder='password'/>
+                <p class="pass-reqs">Password must be at least 8 characters long</p>
                 <button>Login</button>
+                <span>Don't have an account yet?<br /><br /> <Link to="/makeaccount">Make one here.</Link>
+                </span>
             </form>
         </div>
     )
--- client/src/pages/MakeAccount.js
+++ client/src/pages/MakeAccount.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MakeAccount = () => {
     return (
-        <div>MakeAccount</div>
+        <div className = 'authenticate'>
+            <h1>Register Your Account</h1>
+            <form>
+                <input type="email" placeholder='email'/>
+                <input type ="text" placeholder='username'/>
+                <input type ="password" placeholder='password'/>
+                <p class="pass-reqs">Password must be at least 8 characters long and<br /><br />
+                must contain at least one special character.</p>
+                <button>Register</button>
+                <span>Already have an account?<br /><br /> <Link to="/login">Log in here.</Link>
+                </span>
+            </form>
+        </div>
     )
 }
 
