9f91fa9358f9e114f5882596ad979b8adcad3007
Brandon Kiley <b.kiley.113@gmail.com>
2024-03-03 18:32:07 -0800

    changed up the homepage to include a slideshow of images

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,68 @@
 import React from 'react'
 import search_icon from "../images/search_icon.png"
+import image1 from "../images/store.jpg"
+import image2 from "../images/crowd.jpg"
+import image3 from "../images/ppl.webp"
 
-const HomePage = () => {
+// const HomePage = () => {
+//     return (
+//         <div className = 'homepage'>
+//             <input type ="text" placeholder='Search for Products'/>
+//             <img src={search_icon} alt="" />
+//             <button>GO</button>
+//         </div>
+//     )
+// }
+
+// export default HomePage
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const spanStyle = {
+  padding: '20px',
+  background: '#efefef',
+  color: '#000000'
+}
+
+const divStyle = {
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundSize: 'cover',
+  height: '400px'
+}
+const slideImages = [
+  {
+    url: image1,
+    caption: 'Slide 1'
+  },
+  {
+    url: image2,
+    caption: 'Slide 2'
+  },
+  {
+    url: image3,
+    caption: 'Slide 3'
+  },
+];
+
+const Homepage = () => {
     return (
-        <div className = 'homepage'>
-            <input type ="text" placeholder='Search for Products'/>
-            <img src={search_icon} alt="" />
-            <button>GO</button>
-        </div>
+      <div className="slide-container">
+        <input type ="text" placeholder='Search for Products'/>
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div key={index}>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+                <span style={spanStyle}>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+        <span>BEST SELLERS</span>
+      </div>
     )
 }
 
-export default HomePage
+export default Homepage;
