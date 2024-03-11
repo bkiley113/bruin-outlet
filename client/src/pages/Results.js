6e8ff41c4ad6eeed4b03c074ab867a868dc16533
Brandon Kiley <b.kiley.113@gmail.com>
2024-03-11 01:32:18 -0700

    added search results header

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
 import SearchBar from '../components/SearchBar'
 import Gallery from '../components/Gallery'
 
 const Results = ({itemsarr}) => {
+    
     return (
         <div className='results'>
             <SearchBar />
+            <h1>Search Results:</h1>
             <Gallery itemsarr={itemsarr}></Gallery>
         </div>
     )
