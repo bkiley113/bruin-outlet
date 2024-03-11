18eb61e33e49e1763d03773aafffeaf8570ca5d0
John <tmdgjs2592@gmail.com>
2024-03-10 23:27:54 -0700

    Created Otp and refined Login, and Register page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import img from "C:/Users/tmdgj/UCLA/cs35L/project/web-app/client/src/images/demologo.png"
+
 
 const Login = () => {
     return (
         <div className = 'authenticate'>
-            <Link to='/'><div className='bar'>Bar goes here, should be able to get back to homepage</div></Link>
-            <h2></h2>
-            <h1>Login</h1>
-            <form>
-                <input type ="text" placeholder='username'/>
-                <input type ="password" placeholder='password'/>
-                <p className="err-mess">Invalid username or password.</p>
-                <button>Login</button>
-                <span>Don't have an account yet?<br /><br /> <Link to="/makeaccount">Make one here.</Link>
-                </span>
-            </form>
+            <Link to='/'>
+                <a >
+                    <img className="loginlogo" src={img} />
+                </a>
+            </Link>
+            <div className= "auth-container">
+                <form>
+                <h2>Login</h2>
+                    <div className="form_g">
+                        <label>Username</label>
+                        <input type ="text" placeholder='username'/>
+                    </div>
+                    <div className="form_g">
+                        <label>Password</label>
+                        <input type ="password" placeholder='password'/>
+                    </div>
+                    
+                    <p className="err-mess">Invalid username or password.</p>
+                    <button>Login</button>
+                    <span>Don't have an account yet?<br /><br /> 
+                    <Link to="/makeaccount">Make one here.</Link>
+                    </span>
+                </form>
+            </div>
+            
         </div>
     )
 }
 
-export default Login
+export default Login;
