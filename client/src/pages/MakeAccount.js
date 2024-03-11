5b49438fd1b2306ce3af1ae1dc75b565ed8a1364
John <tmdgjs2592@gmail.com>
2024-03-10 23:27:54 -0700

    Created Otp and refined Login, and Register page

--- client/src/pages/MakeAccount.js
+++ client/src/pages/MakeAccount.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import img from "C:/Users/tmdgj/UCLA/cs35L/project/web-app/client/src/images/demologo.png"
 
 const MakeAccount = () => {
     return (
         <div className = 'authenticate'>
-            <Link to='/'><div className='bar'>Bar goes here, should be able to get back to homepage</div></Link>
-            <h1>Register Your Account</h1>
-            <form>
-                <input type="email" placeholder='email'/>
-                <input type ="text" placeholder='username'/>
-                <input type ="password" placeholder='password'/>
-                <p class="pass-reqs">Password must be at least 8 characters long and<br /><br />
-                must contain at least one special character.</p>
-                <button>Register</button>
-                <span>Already have an account?<br /><br /> <Link to="/login">Log in here.</Link>
-                </span>
-            </form>
+            <Link to='/'>
+                <a >
+                    <img className="loginlogo" src={img} />
+                </a>
+            </Link>
+            <div className= "auth-container">
+                <form>
+                    <h2>Register</h2>
+                        <div className="form_g_r">
+                            <label>Email</label>
+                            <input type="email" placeholder='email'/>
+                        </div>
+                        <div className="form_g_r">
+                            <label>Username</label>
+                            <input type ="text" placeholder='username'/>
+                            
+                        </div>
+                        <div className="form_g_r">
+                            <label>Password</label>
+                            <input type ="password" placeholder='password'/>
+                            </div>
+                        <p class="pass-reqs">Password must be at least 8 characters long and<br />
+                        must contain at least one special character.</p>
+                        <button>Register</button>
+                        <span>Already have an account?<br /><br /> <Link to="/login">Log in here.</Link>
+                        </span>
+                </form>
+            </div>
+            
         </div>
     )
 }
